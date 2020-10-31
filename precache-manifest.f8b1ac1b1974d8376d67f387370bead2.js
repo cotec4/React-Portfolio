9cb4b8095f747734e986e0f1f33867a94034fe37
Connor Cote <cotec4@gmail.com>
2020-10-31 13:48:20 -0400

    Updates

---
+++ precache-manifest.f8b1ac1b1974d8376d67f387370bead2.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336beae07b7d26e30996d987364b4fc6",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "693b7086051996147d20",
+    "url": "/React-Portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "54204fbba4d28767ecb9",
+    "url": "/React-Portfolio/static/css/main.01fbe530.chunk.css"
+  },
+  {
+    "revision": "693b7086051996147d20",
+    "url": "/React-Portfolio/static/js/2.058847b4.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/React-Portfolio/static/js/2.058847b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54204fbba4d28767ecb9",
+    "url": "/React-Portfolio/static/js/main.f1ba7d7e.chunk.js"
+  },
+  {
+    "revision": "ed2db9984f9f1e943e82",
+    "url": "/React-Portfolio/static/js/runtime-main.fbcb4e55.js"
+  },
+  {
+    "revision": "9c6f82d06a3b14a6e81abad72bc44144",
+    "url": "/React-Portfolio/static/media/Francisco_Partners1.9c6f82d0.png"
+  },
+  {
+    "revision": "a8c8b1969a75d03d5c6af9cfd79db0ad",
+    "url": "/React-Portfolio/static/media/baml.a8c8b196.jpg"
+  },
+  {
+    "revision": "f126b772f8035f8607bcaf3cce9a5395",
+    "url": "/React-Portfolio/static/media/beers.f126b772.jpg"
+  },
+  {
+    "revision": "c502d605a07d9b7d881fef49c9174e72",
+    "url": "/React-Portfolio/static/media/boston-college.c502d605.png"
+  },
+  {
+    "revision": "89a523f7d28bf42e1e9da92a49f0cbdd",
+    "url": "/React-Portfolio/static/media/covidImpact.89a523f7.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/React-Portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6c415bdf05c87b4c14bc14cef43fdb67",
+    "url": "/React-Portfolio/static/media/homework.6c415bdf.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/React-Portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/React-Portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/React-Portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/React-Portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/React-Portfolio/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/React-Portfolio/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "8e6608561d26dd9aea11aa2f59280642",
+    "url": "/React-Portfolio/static/media/marist.8e660856.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/React-Portfolio/static/media/myImage.bd80794f.png"
+  }
+]);
