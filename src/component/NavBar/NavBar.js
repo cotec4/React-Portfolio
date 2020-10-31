e9aa802e4414dd006a35cd0db82abe7132e9d59e
Connor Cote <cotec4@gmail.com>
2020-10-30 23:10:35 -0400

    styling not working throughout

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,38 +1,36 @@
 import React from 'react';
-import "../bootstrap.css";
 import "../font-awesome.css";
 import "../styles.scss";
+import "../bootstrap.css";
 
 function NavBar() {
     return (
-        <header>
-            <div id="mobile-menu-close">
-                <span>Close</span> <i className="fa fa-times" aria-hidden="true"></i>
-                <ul id="menu" className="shadow">
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#experience">Experience</a>
-                    </li>
-                    <li>
-                        <a href="#education">Education</a>
-                    </li>
-                    <li>
-                        <a href="#projects">Projects</a>
-                    </li>
-                    <li>
-                        <a href="#homework">Homework</a>
-                    </li>
-                    <li>
-                        <a href="#skills">Skills</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </header>
+        <div id="mobile-menu-close">
+            <span>Close</span> <i className="fa fa-times" aria-hidden="true"></i>
+            <ul id="menu" className="shadow">
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+                <li>
+                    <a href="#education">Education</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#homework">Homework</a>
+                </li>
+                <li>
+                    <a href="#skills">Skills</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
     )
 }
 
